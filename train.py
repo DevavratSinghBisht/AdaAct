ec132aa435a1d975c940c8765389108a2191d73c
DevavratSinghBisht <devavratsinghbisht@gmail.com>
2024-11-22 13:34:09 -0800

    enhanced logging and saving history

--- train.py
+++ train.py
@@ -6,6 +6,8 @@ from utils import count_parameters, get_model, get_num_classes, get_dataloader
 
 import argparse
 import logging
+import datetime
+import json
 
 
 def train(model, dataloader, num_epochs, device, logger, filename):
@@ -20,8 +22,16 @@ def train(model, dataloader, num_epochs, device, logger, filename):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+    history = {
+        'train_loss'        : [],
+        'train_accuracy'    : [],
+        'test_loss'         : [],
+        'test_accuracy'     : []
+    }
+
     # Training Loop
     for epoch in range(num_epochs):
+        train_start_time = datetime.datetime.now()
         model.train()
         running_loss = 0.0
         correct=0
@@ -44,29 +54,43 @@ def train(model, dataloader, num_epochs, device, logger, filename):
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
+        train_loss = running_loss/len(train_loader)
         train_accuracy = 100 * correct / total
+        train_time = datetime.datetime.now() - train_start_time
+        logger.info(f"Epoch [{epoch+1}/{num_epochs}], Loss: {train_loss:.4f}, Training Accuracy : {train_accuracy}, Train Time {train_time}")
 
-        logger.info(f"Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(train_loader):.4f}, Training Accuracy : {train_accuracy}")
+        model.eval()
+        running_loss = 0.0
+        correct = 0
+        total = 0
+        test_start_time = datetime.datetime.now()
+        
+        with torch.no_grad():
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                loss = criterion(outputs, labels)
+                running_loss += loss.item()
 
-    model.eval()
-    correct = 0
-    total = 0
+        test_loss = running_loss/len(train_loader)
+        test_accuracy = 100 * correct / total
+        test_time = datetime.datetime.now()-test_start_time
 
-    with torch.no_grad():
-        for images, labels in test_loader:
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+        logger.info(f"Epoch [{epoch+1}/{num_epochs}], Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.2f}, Test Time {test_time}")
 
-    accuracy = 100 * correct / total
-
-    logger.info(f"Test Accuracy: {accuracy:.2f}%")
+        history['train_loss'].append(train_loss)
+        history['train_accuracy'].append(train_accuracy)
+        history['test_loss'].append(test_loss)
+        history['test_accuracy'].append(test_accuracy)
 
     torch.save(model.state_dict(), f'{filename}.pth')
+    with open(f"{filename}.json", "w") as outfile:
+        json.dump(history, outfile, indent=4)
 
-    return model
+    return model, history
 
 if __name__ == "__main__":
     
@@ -76,7 +100,7 @@ if __name__ == "__main__":
     parser.add_argument('--dataset', default='cifar100', type=str)
     parser.add_argument('--epochs', default=100, type = int)
     parser.add_argument('--gpu-index', default=0, type=int, help='Gpu index.')
-    parser.add_argument('--seed', default=69, type=int, help='Random ssed.')
+    parser.add_argument('--seed', default=69, type=int, help='Random seed.')
 
     args = parser.parse_args()
 
@@ -106,4 +130,4 @@ if __name__ == "__main__":
         logger.info(f'Created {args.model} model with parameters {count_parameters(model)}')
         logger.info(model)
         dataloader = get_dataloader(args.dataset)
-        model = train(model, dataloader, args.epochs, device, logger, filename)
+        model, history = train(model, dataloader, args.epochs, device, logger, filename)
