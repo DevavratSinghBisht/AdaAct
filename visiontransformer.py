edd816718db913b070a31395515263a0963beb2e
vrucodes <VruCodes@24>
2024-11-24 17:12:09 -0800

    Added Vision Transformer's refactored code with AdaAct-2

--- visiontransformer.py
+++ visiontransformer.py
@@ -1,83 +1,393 @@
+# Commented out IPython magic to ensure Python compatibility.
 import torch
-import torch.nn as nn
-from torchvision import datasets, transforms
+from torch import nn
+from torch import functional as F
+from torch import optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import math
+import matplotlib.pyplot as plt
+from adaptive_activation import AdaAct
 
-from resnet import ResNet18
-from visiontransformer import VisionTransformer
-
-all_acts = [
-    nn.ReLU(),
-    nn.LeakyReLU(),
-    nn.PReLU(),
-    nn.ELU(),
-    nn.SELU(),
-    nn.GELU(),
-    nn.Tanh(),
-    nn.Sigmoid(),
-    nn.Softplus(),
-    nn.Softsign(),
-    nn.SiLU(),
-    nn.Hardswish(),
-    nn.Hardtanh(),
-    nn.Hardsigmoid(),
-    nn.LogSigmoid(),
-    nn.Threshold(0.5, 0.0),
-    nn.Softmax(dim=-1),
-    nn.LogSoftmax(dim=-1)
-]
+# %matplotlib inline
 
 
-def get_num_classes(dataset):
-    if dataset == 'cifar10':
-        return 10
-    elif dataset == 'cifar100':
-        return 100
+class Attention(nn.Module):
+    '''
+    Attention Module used to perform self-attention operation allowing the model to attend
+    information from different representation subspaces on an input sequence of embeddings.
+    The sequence of operations is as follows :-
 
+    Input -> Query, Key, Value -> ReshapeHeads -> Query.TransposedKey -> Softmax -> Dropout
+    -> AttentionScores.Value -> ReshapeHeadsBack -> Output
 
-def get_model(model_name, num_classes, adaact):
-    if model_name == 'resnet18':
-        if adaact:
-            model = ResNet18(num_classes, all_acts)
+    Args:
+        embed_dim: Dimension size of the hidden embedding
+        heads: Number of parallel attention heads (Default=8)
+        activation: Optional activation function to be applied to the input while
+                    transforming to query, key and value matrixes (Default=None)
+        dropout: Dropout value for the layer on attention_scores (Default=0.1)
+
+    Methods:
+        _reshape_heads(inp) :-
+        Changes the input sequence embeddings to reduced dimension according to the number
+        of attention heads to parallelize attention operation
+        (batch_size, seq_len, embed_dim) -> (batch_size * heads, seq_len, reduced_dim)
+
+        _reshape_heads_back(inp) :-
+        Changes the reduced dimension due to parallel attention heads back to the original
+        embedding size
+        (batch_size * heads, seq_len, reduced_dim) -> (batch_size, seq_len, embed_dim)
+
+        forward(inp) :-
+        Performs the self-attention operation on the input sequence embedding.
+        Returns the output of self-attention as well as atttention scores
+        (batch_size, seq_len, embed_dim) -> (batch_size, seq_len, embed_dim), (batch_size * heads, seq_len, seq_len)
+
+    Examples:
+        >>> attention = Attention(embed_dim, heads, activation, dropout)
+        >>> out, weights = attention(inp)
+    '''
+    def __init__(self, embed_dim, heads=8, activation=nn.ReLU(), dropout=0.1):
+        super(Attention, self).__init__()
+        self.heads = heads
+        self.embed_dim = embed_dim
+        self.query = nn.Linear(embed_dim, embed_dim)
+        self.key = nn.Linear(embed_dim, embed_dim)
+        self.value = nn.Linear(embed_dim, embed_dim)
+        self.softmax = nn.Softmax(dim=-1)
+        if type(activation) == list:
+            for act in activation:
+                assert isinstance(act, nn.Module)
+            self.activation1 = AdaAct(activation)
         else:
-            model = ResNet18(num_classes)
-    if model_name == 'visionTransformer':
-        if adaact:
-            model = VisionTransformer(num_classes, all_acts)
+            self.activation1 = activation
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, inp):
+        # inp: (batch_size, seq_len, embed_dim)
+        batch_size, seq_len, embed_dim = inp.size()
+        assert embed_dim == self.embed_dim
+
+        query = self.activation1(self.query(inp))
+        key   = self.activation1(self.key(inp))
+        value = self.activation1(self.value(inp))
+
+        # output of _reshape_heads(): (batch_size * heads, seq_len, reduced_dim) | reduced_dim = embed_dim // heads
+        query = self._reshape_heads(query)
+        key   = self._reshape_heads(key)
+        value = self._reshape_heads(value)
+
+        # attention_scores: (batch_size * heads, seq_len, seq_len) | Softmaxed along the last dimension
+        attention_scores = self.softmax(torch.matmul(query, key.transpose(1, 2)))
+
+        # out: (batch_size * heads, seq_len, reduced_dim)
+        out = torch.matmul(self.dropout(attention_scores), value)
+
+        # output of _reshape_heads_back(): (batch_size, seq_len, embed_size)
+        out = self._reshape_heads_back(out)
+
+        return out, attention_scores
+
+    def _reshape_heads(self, inp):
+        # inp: (batch_size, seq_len, embed_dim)
+        batch_size, seq_len, embed_dim = inp.size()
+
+        reduced_dim = self.embed_dim // self.heads
+        assert reduced_dim * self.heads == self.embed_dim
+        out = inp.reshape(batch_size, seq_len, self.heads, reduced_dim)
+        out = out.permute(0, 2, 1, 3)
+        out = out.reshape(-1, seq_len, reduced_dim)
+
+        # out: (batch_size * heads, seq_len, reduced_dim)
+        return out
+
+    def _reshape_heads_back(self, inp):
+        # inp: (batch_size * heads, seq_len, reduced_dim) | reduced_dim = embed_dim // heads
+        batch_size_mul_heads, seq_len, reduced_dim = inp.size()
+        batch_size = batch_size_mul_heads // self.heads
+
+        out = inp.reshape(batch_size, self.heads, seq_len, reduced_dim)
+        out = out.permute(0, 2, 1, 3)
+        out = out.reshape(batch_size, seq_len, self.embed_dim)
+
+        # out: (batch_size, seq_len, embed_dim)
+        return out
+
+# Check if Dropout should be used after second Linear Layer
+class FeedForward(nn.Module):
+    '''
+    FeedForward Network with two sequential linear layers with GELU activation function
+    ,applied to the output of self attention operation. The sequence of operations is as
+    follows :-
+
+    Input -> FC1 -> GELU -> Dropout -> FC2 -> Output
+
+    Args:
+        embed_dim: Dimension size of the hidden embedding
+        forward_expansion: The scale used to transform the input embedding to a higher dimension
+                           and then scaled back to capture richer information (Default=1)
+        dropout: Dropout value for the layer on attention_scores (Default=0.1)
+
+    Methods:
+        forward(inp) :-
+        Applies the sequence of operations mentioned above.
+        (batch_size, seq_len, embed_dim) -> (batch_size, seq_len, embed_dim)
+
+    Examples:
+        >>> FF = FeedForward(8, 1)
+        >>> out = FF(inp)
+    '''
+    def __init__(self, embed_dim, activation = nn.GELU(), forward_expansion=1, dropout=0.1):
+        super(FeedForward, self).__init__()
+        self.embed_dim = embed_dim
+        self.fc1 = nn.Linear(embed_dim, embed_dim * forward_expansion)
+        self.fc2 = nn.Linear(embed_dim * forward_expansion, embed_dim)
+        self.dropout = nn.Dropout(dropout)
+        if type(activation) == list:
+            for act in activation:
+                assert isinstance(act, nn.Module)
+            self.activation1 = AdaAct(activation)
         else:
-            model = VisionTransformer(num_classes)
-
-    return model
+            self.activation1 = nn.GELU()
 
 
-def get_dataloader(dataset):
-    # CIFAR-100 Dataset and DataLoader
-    train_transform = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomCrop(32, padding=4),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+    def forward(self, inp):
+        # inp: (batch_size, seq_len, embed_dim)
+        batch_size, seq_len, embed_dim = inp.size()
+        assert embed_dim == self.embed_dim
 
-    test_transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+        out = self.dropout(self.activation1(self.fc1(inp)))
+        # out = self.dropout(self.fc2(out))
+        out = self.fc2(out)
 
-    if dataset == 'cifar10':
-        train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
-        test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
-    elif dataset == 'cifar100':
-        train_dataset = datasets.CIFAR100(root='./data', train=True, download=True, transform=train_transform)
-        test_dataset = datasets.CIFAR100(root='./data', train=False, download=True, transform=test_transform)
+        # out: (batch_size, seq_len, embed_dim)
+        return out
 
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=128, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=128, shuffle=False)
+class TransformerBlock(nn.Module):
+    '''
+    Transformer Block combines both the attention module and the feed forward module with layer
+    normalization, dropout and residual connections. The sequence of operations is as follows :-
 
-    return train_loader, test_loader
+    Input -> LayerNorm1 -> Attention -> Residual -> LayerNorm2 -> FeedForward -> Output
+      |                                   |  |                                      |
+      |-------------Addition--------------|  |---------------Addition---------------|
+
+    Args:
+        embed_dim: Dimension size of the hidden embedding
+        heads: Number of parallel attention heads (Default=8)
+        activation: Optional activation function to be applied to the input while
+                    transforming to query, key and value matrixes (Default=None)
+        forward_expansion: The scale used to transform the input embedding to a higher dimension
+                           and then scaled back to capture richer information (Default=1)
+        dropout: Dropout value for the layer on attention_scores (Default=0.1)
+
+    Methods:
+        forward(inp) :-
+        Applies the sequence of operations mentioned above.
+        (batch_size, seq_len, embed_dim) -> (batch_size, seq_len, embed_dim)
+
+    Examples:
+        >>> TB = TransformerBlock(embed_dim, heads, activation, forward_expansion, dropout)
+        >>> out = TB(inp)
+    '''
+    def __init__(self, embed_dim, heads=8, activation=nn.ReLU(), forward_expansion=1, dropout=0.1):
+        super(TransformerBlock, self).__init__()
+        self.embed_dim = embed_dim
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.attention = Attention(embed_dim, heads, activation, dropout)
+        self.norm2 = nn.LayerNorm(embed_dim)
+        self.feed_forward = FeedForward(embed_dim, activation, forward_expansion, dropout)
+
+    def forward(self, inp):
+        # inp: (batch_size, seq_len, embed_dim)
+        batch_size, seq_len, embed_dim = inp.size()
+        assert embed_dim == self.embed_dim
+
+        res = inp
+        out = self.norm1(inp)
+        out, _ = self.attention(out)
+        out = out + res
+
+        res = out
+        out = self.norm2(out)
+        out = self.feed_forward(out)
+        out = out + res
+
+        # out: (batch_size, seq_len, embed_dim)
+        return out
+
+class Transformer(nn.Module):
+    '''
+    Transformer combines multiple layers of Transformer Blocks in a sequential manner. The sequence
+    of the operations is as follows -
+
+    Input -> TB1 -> TB2 -> .......... -> TBn (n being the number of layers) -> Output
+
+    Args:
+        embed_dim: Dimension size of the hidden embedding
+        layers: Number of Transformer Blocks in the Transformer
+        heads: Number of parallel attention heads (Default=8)
+        activation: Optional activation function to be applied to the input while
+                    transforming to query, key and value matrixes (Default=None)
+        forward_expansion: The scale used to transform the input embedding to a higher dimension
+                           and then scaled back to capture richer information (Default=1)
+        dropout: Dropout value for the layer on attention_scores (Default=0.1)
+
+    Methods:
+        forward(inp) :-
+        Applies the sequence of operations mentioned above.
+        (batch_size, seq_len, embed_dim) -> (batch_size, seq_len, embed_dim)
+
+    Examples:
+        >>> transformer = Transformer(embed_dim, layers, heads, activation, forward_expansion, dropout)
+        >>> out = transformer(inp)
+    '''
+    def __init__(self, embed_dim, layers, heads=8, activation=nn.ReLU(), forward_expansion=1, dropout=0.1):
+        super(Transformer, self).__init__()
+        self.embed_dim = embed_dim
+        self.trans_blocks = nn.ModuleList(
+            [TransformerBlock(embed_dim, heads, activation, forward_expansion, dropout) for i in range(layers)]
+        )
+
+    def forward(self, inp):
+        # inp: (batch_size, seq_len, embed_dim)
+
+        out = inp
+        for block in self.trans_blocks:
+            out = block(out)
+
+        # out: (batch_size, seq_len, embed_dim)
+        return out
+
+# Not Exactly Same as Paper
+class ClassificationHead(nn.Module):
+    '''
+    Classification Head attached to the first sequence token which is used as the arbitrary
+    classification token and used to optimize the transformer model by applying Cross-Entropy
+    loss. The sequence of operations is as follows :-
+
+    Input -> FC1 -> GELU -> Dropout -> FC2 -> Output
+
+    Args:
+        embed_dim: Dimension size of the hidden embedding
+        classes: Number of classification classes in the dataset
+        dropout: Dropout value for the layer on attention_scores (Default=0.1)
+
+    Methods:
+        forward(inp) :-
+        Applies the sequence of operations mentioned above.
+        (batch_size, embed_dim) -> (batch_size, classes)
+
+    Examples:
+        >>> CH = ClassificationHead(embed_dim, classes, dropout)
+        >>> out = CH(inp)
+    '''
+    def __init__(self, embed_dim, classes, dropout=0.1):
+        super(ClassificationHead, self).__init__()
+        self.embed_dim = embed_dim
+        self.classes = classes
+        self.fc1 = nn.Linear(embed_dim, embed_dim // 2)
+        self.activation = nn.GELU()
+        self.fc2 = nn.Linear(embed_dim // 2, classes)
+        self.softmax = nn.Softmax(dim=-1)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, inp):
+        # inp: (batch_size, embed_dim)
+        batch_size, embed_dim = inp.size()
+        assert embed_dim == self.embed_dim
+
+        out = self.dropout(self.activation(self.fc1(inp)))
+        # out = self.softmax(self.fc2(out))
+        out = self.fc2(out)
+
+        # out: (batch_size, classes)
+        return out
+
+class VisionTransformer(nn.Module):
+    '''
+    Vision Transformer is the complete end to end model architecture which combines all the above modules
+    in a sequential manner. The sequence of the operations is as follows -
+
+    Input -> CreatePatches -> ClassToken, PatchToEmbed , PositionEmbed -> Transformer -> ClassificationHead -> Output
+                                   |            | |                |
+                                   |---Concat---| |----Addition----|
+
+    Args:
+        patch_size: Length of square patch size
+        max_len: Max length of learnable positional embedding
+        embed_dim: Dimension size of the hidden embedding
+        classes: Number of classes in the dataset
+        layers: Number of Transformer Blocks in the Transformer
+        channels: Number of channels in the input (Default=3)
+        heads: Number of parallel attention heads (Default=8)
+        activation: Optional activation function to be applied to the input while
+                    transforming to query, key and value matrixes (Default=None)
+        forward_expansion: The scale used to transform the input embedding to a higher dimension
+                           and then scaled back to capture richer information (Default=1)
+        dropout: Dropout value for the layer on attention_scores (Default=0.1)
+
+    Methods:
+        forward(inp) :-
+        Applies the sequence of operations mentioned above.
+        It outputs the classification output as well as the sequence output of the transformer
+        (batch_size, channels, width, height) -> (batch_size, classes), (batch_size, seq_len+1, embed_dim)
+
+    Examples:
+        >>> ViT = VisionTransformer(patch_size, max_len, embed_dim, classes, layers, channels, heads, activation, forward_expansion, dropout)
+        >>> class_out, hidden_seq = ViT(inp)
+    '''
+    def __init__(self, activation=nn.ReLU(), patch_size=4, max_len=100, embed_dim=512, classes=100, layers=8, channels=3, heads=8, forward_expansion=1, dropout=0.1):
+        super(VisionTransformer, self).__init__()
+        self.name = 'VisionTransformer'
+        self.patch_size = patch_size
+        self.embed_dim = embed_dim
+        self.channels = channels
+        self.patch_to_embed = nn.Linear(patch_size * patch_size * channels, embed_dim)
+        self.position_embed = nn.Parameter(torch.randn((max_len, embed_dim)))
+        self.transformer = Transformer(embed_dim, layers, heads, activation, forward_expansion, dropout)
+        self.classification_head = ClassificationHead(embed_dim, classes)
+        self.class_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+
+    def forward(self, inp):
+        # inp: (batch_size, channels, width, height)
+        batch_size, channels, width, height = inp.size()
+        assert channels == self.channels
+
+        out = inp.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size).contiguous()
+        out = out.view(batch_size, channels, -1, self.patch_size, self.patch_size)
+        out = out.permute(0, 2, 3, 4, 1)
+        # out: (batch_size, seq_len, patch_size, patch_size, channels) | seq_len would be (width*height)/(patch_size**2)
+        batch_size, seq_len, patch_size, _, channels = out.size()
+
+        out = out.reshape(batch_size, seq_len, -1)
+        out = self.patch_to_embed(out)
+        # out: (batch_size, seq_len, embed_dim)
+
+        class_token = self.class_token.expand(batch_size, -1, -1)
+        out = torch.cat([class_token, out], dim=1)
+        # out: (batch_size, seq_len+1, embed_dim)
+
+        position_embed = self.position_embed[:seq_len+1]
+        position_embed = position_embed.unsqueeze(0).expand(batch_size, seq_len+1, self.embed_dim)
+        out = out + position_embed
+        # out: (batch_size, seq_len+1, embed_dim) | Added Positional Embeddings
+
+        out = self.transformer(out)
+        # out: (batch_size, seq_len+1, embed_dim)
+        class_token = out[:, 0]
+        # class_token: (batch_size, embed_dim)
+
+        class_out = self.classification_head(class_token)
+        # class_out: (batch_size, classes)
+
+        return class_out, out
 
 
-def count_parameters(model):
-    total_params = 0
-    for param in model.parameters():
-        total_params += param.numel()
-    return total_params
+
+
+
+
